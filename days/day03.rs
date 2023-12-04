7bfaad27a7840b6d9565fd7a789e615c16c570fb
Michael Boyle <michael.boyle@studiogobo.com>
2023-12-04 14:58:40 +0000

    Progress work on Day 3, will need to build later

--- days/day03.rs
+++ days/day03.rs
@@ -1,6 +1,43 @@
+use std::fs;
 
+struct Part
+{
+    x_loc : usize,
+    y_loc : usize,
+    val : char
+}
+
+fn gather_parts(data : &str) -> Vec<Part>
+{
+    let mut parts : Vec<Part> = Vec::new();
+    for (y, dataline) in data.split("\n").enumerate()
+    {
+        for (x, character) in dataline.chars().enumerate()
+        {
+            if character != '.' && !character.is_digit(10)
+            {
+                parts.push(Part
+                {
+                    x_loc: x,
+                    y_loc: y,
+                    val: character
+                });
+            }
+        }
+    }
+
+    parts
+}
 
 pub fn main()
 {
+    let data = fs::read_to_string("inputs/day02.txt").expect("Failed to read file!");
+    let parts = gather_parts(&data);
 
+    for part in parts
+    {
+        println!("{}", part.val);
+    }
+
+    //println!("Part 1: {value}");
 }
